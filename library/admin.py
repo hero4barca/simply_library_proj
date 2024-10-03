c2b603688048bda0bcbf0b688f46ac793ad12939
Hero <hero4barca@gmail.com>
2024-10-03 14:30:19 +0100

    admin file updated

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import Author, Book
+
+
 # Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["title", "published_on", "get_authors" ]
+
+    def get_authors(self, obj):
+        authors = ""
+        for author in obj.author:
+            authors + str(author) + "; "
+        return authors
+    get_authors.short_description = "author(s)"
