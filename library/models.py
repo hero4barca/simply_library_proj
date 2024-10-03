618c41b529f4d4a03180ee90d3dd2233d298a429
Hero <hero4barca@gmail.com>
2024-10-03 13:48:15 +0100

    simple book model for library app

--- library/models.py
+++ library/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=20 )
+    last_name = models.CharField(max_length=20 )
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    published_on = models.DateTimeField(auto_now_add=True)
+    author = models.ManyToManyField(Author)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self) -> str:
+        return f'{self.title}; {self.published_on}'
